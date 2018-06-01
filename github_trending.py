d74b4198efbf241b15bf4b4340f7b36b7c1b85f1
esgzk <everythingseemsgood@gmail.com>
2018-06-02 00:23:30 +0300

    intermediate implementatiton

--- github_trending.py
+++ github_trending.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 
+
 def shifted_time(days):
     current_date = datetime.datetime.now()
     new_date = datetime.timedelta(days=days)
@@ -8,28 +9,37 @@ def shifted_time(days):
     return current_date - new_date
 
 
-def get(date):
+def get_users(date):
+    users = []
     formated_date = date.strftime("%Y-%m-%d")
     params = {
         'q': str(formated_date),
-        'sort':'starts',
-        'order':'desc'
+        'sort': 'starts',
+        'order': 'desc'
     }
     r = requests.get("https://api.github.com/search/repositories", params=params)
-    repository_name = r.json()["items"][0]["name"]
-    login_name = r.json()["items"][0]["owner"]["login"]
-    return repository_name, login_name
+    for i in range(20):
+        login_name = r.json()["items"][i]["owner"]["login"]
+        repository_name = r.json()["items"][i]["name"]
+        user = (login_name, repository_name)
+        users.append(user)
+
+    return users
 
 
-def get_issues(repos_name, login_name):
-    url = "https://api.github.com/repos/{0}/{1}/issues".format(login_name, repos_name)
-    print(url)
-    #r = requests.get(url)
-    #return print(r.json())
+def get_issues(users):
+    for i in range(20):
+        login_name = users[i][0]
+        repos_name = users[i][1]
+        url = "https://api.github.com/repos/{0}/{1}/issues?q=state:open+type:issue&sort=created&order=desc".format(
+            login_name, repos_name)
+        r = requests.get(url)
+        for g in range(len(r.json())):
+            title = r.json()[g]["title"]
+            # text = r.json()[g]["body"]  body of issue if needed
+            print(f"\t\t#{g:#d} issue includes {title}\n")
+    return 0
 
 
 if __name__ == "__main__":
-    list_of_names = get(shifted_time(7))
-    get_issues(list_of_names[0], list_of_names[1])
-
-
+    get_issues(get_users(shifted_time(0)))
