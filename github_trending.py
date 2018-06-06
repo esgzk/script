3bcd701ad0e43c01ac04fa634d48c0f06e5cc6c6
esgzk <everythingseemsgood@gmail.com>
2018-06-06 17:30:45 +0300

    Another very interesting task was done by esgzk

--- github_trending.py
+++ github_trending.py
@@ -12,12 +12,12 @@ def shifted_time(days):
 def get_users(date):
     users = []
     formated_date = date.strftime("%Y-%m-%d")
-    params = {
+    params_for_users = {
         'q': str(formated_date),
-        'sort': 'stars',
-        'order': 'desc'
+        "sort": "stars",
+        "order": "desc"
     }
-    r = requests.get("https://api.github.com/search/repositories", params=params)
+    r = requests.get("https://api.github.com/search/repositories", params=params_for_users)
     for i in range(20):
         login_name = r.json()["items"][i]["owner"]["login"]
         repository_name = r.json()["items"][i]["name"]
@@ -28,20 +28,24 @@ def get_users(date):
 
 
 def get_issues(users):
-    params_for_oauth = {
-        "username": "esgzk"
+    params_for_issues = {
+        'q': "state:open+type:issue",
+        "sort": "created",
+        "order": "desc",
+        "token": "your-personal-access-github-token"
     }
-    requests.get("https://api.github.com/user", params=params_for_oauth)
     for user in users:
         login_name, repos_name = user[0], user[1]
-        url = "https://api.github.com/repos/{0}/{1}/issues?q=state:open+type:issue&sort=created&order=desc".format(
-            login_name, repos_name)
-        r = requests.get(url)
+        url = "https://api.github.com/repos/{0}/{1}/issues".format(login_name, repos_name)
+        r = requests.get(url, params=params_for_issues)
         issues = r.json()
-        print("\"{0}'s:\" issues\n".format(repos_name))
-        for issue in issues:
-            title = issue["title"]
-            print(f"\t\t issue includes:\" {title}\"")
+        if len(issues):
+            print("\"{0}'s:\"".format(repos_name))
+            for issue in issues:
+                title = issue["title"]
+                print(f"\t\t issue includes:\"{title}\"")
+        else:
+            print("\"{0}'s\": No opened issues\n".format(repos_name))
 
 
 if __name__ == "__main__":
